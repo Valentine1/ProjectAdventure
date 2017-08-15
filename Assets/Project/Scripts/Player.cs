2fb2a6113d3f2ab3f95d7fc6a6c40d1063aa07af
Valentine1 <devnetdot@gmail.com>
2017-08-15 14:33:30 +0300

    first commit moving sword - tutorial version

---
+++ Assets/Project/Scripts/Player.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [Header("Visuals")]
+    public GameObject Model;
+
+    [Header("Movement")]
+    public float movingVelocity = 10f;
+    public float jumpingVelocity = 10f;
+    public float rotatingSpeed = 10f;
+
+    [Header("Weapons")]
+    public Sword mainSword;
+
+    private Rigidbody playerBody;
+    private bool canJump = true;
+    private Quaternion targetRotation;
+    // Use this for initialization
+    void Start()
+    {
+        targetRotation = Quaternion.Euler(0, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerBody = this.GetComponent<Rigidbody>();
+        RaycastHit hit;
+        if (Physics.Raycast(this.transform.position, Vector3.down, out hit, 1.01f))
+        {
+            canJump = true;
+        }
+        Model.transform.rotation = Quaternion.Lerp(Model.transform.rotation, targetRotation, rotatingSpeed * Time.deltaTime);
+        ProcessInput();
+    }
+
+    private void ProcessInput()
+    {
+        playerBody.velocity = new Vector3(
+                  0,
+                  playerBody.velocity.y,
+                 0
+                );
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            playerBody.velocity = new Vector3(
+                  movingVelocity,
+                  playerBody.velocity.y,
+                  playerBody.velocity.z
+                );
+            targetRotation = Quaternion.Euler(0, -90, 0);
+            //Model.transform.rotation = Quaternion.Lerp(Model.transform.rotation, Quaternion.Euler(0, -90, 0), rotatingSpeed * Time.deltaTime);
+            //Model.transform.localEulerAngles = new Vector3(0, -90, 0);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            playerBody.velocity = new Vector3(
+                  -movingVelocity,
+                  playerBody.velocity.y,
+                  playerBody.velocity.z
+                );
+            targetRotation = Quaternion.Euler(0, 90, 0);
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            playerBody.velocity = new Vector3(
+                 playerBody.velocity.z,
+                 playerBody.velocity.y,
+                 movingVelocity
+                );
+            targetRotation = Quaternion.Euler(0, 180, 0);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            playerBody.velocity = new Vector3(
+                playerBody.velocity.z,
+                playerBody.velocity.y,
+                -movingVelocity
+               );
+            targetRotation = Quaternion.Euler(0, 0, 0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && canJump)
+        {
+            canJump = false;
+            //this.GetComponent<Rigidbody>().AddForce(0, jumpingForce, 0);
+            playerBody.velocity = new Vector3(
+                 playerBody.velocity.x,
+                 jumpingVelocity,
+                 playerBody.velocity.z
+                );
+        }
+
+        if (Input.GetKeyDown(KeyCode.Z)) {
+            mainSword.Attack();
+        }
+    }
+
+
+}
---
+++ Assets/Project/Scripts/Sword.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sword : MonoBehaviour
+{
+
+    public float swingingSpeed = 6f;
+    public float coolDownSpeed = 2f;
+    public float attackDuration = 0.25f; // time for moving sword forward, after it expires sword moves back
+
+    public float coolDownDuration = 0.5f;  // time for full motion - forward and back 
+
+    private Quaternion targetRotation;
+    private float coolDownTimer = 0f;
+
+    // Use this for initialization
+    void Start()
+    {
+        targetRotation = Quaternion.Euler(330, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.localRotation = Quaternion.Slerp(this.transform.localRotation, targetRotation, swingingSpeed * Time.deltaTime);
+        coolDownTimer -= Time.deltaTime;
+    }
+
+    public void Attack()
+    {
+        if (coolDownTimer <= 0)
+        {
+            targetRotation = Quaternion.Euler(270, 0, 0);
+            coolDownTimer = coolDownDuration;
+            StartCoroutine(CooldownWait());
+        }
+     
+    }
+
+    private IEnumerator CooldownWait()
+    {
+        yield return new WaitForSeconds(attackDuration);
+        targetRotation = Quaternion.Euler(330, 0, 0);
+    }
+}
